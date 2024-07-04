39149bf0fc8032f34a03b0a83b5d32621bc5299d
AlessioConsolo <alessioconsolo3@gmail.com>
2024-07-04 19:16:22 +0200

    [UPD - Palindroma - JS] Added if condition

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -4,3 +4,9 @@ function Palindroma(parola) {
   parola = parola.toLowerCase();
   return parola === parola.split("").reverse().join("");
 }
+
+if (Palindroma(parola)) {
+  console.log(`'${parola}' è una parola palindroma`);
+} else {
+  console.log(`'${parola}' non è una parola palindroma`);
+}
